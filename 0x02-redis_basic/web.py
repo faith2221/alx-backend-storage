cbcfa2a84d15d875087491ac968b73d1f8d182fc
faith2221 <edithbanda014@gmail.com>
2024-05-15 15:36:28 +0000

    Correction

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,23 +4,38 @@ A module with tools for request caching and tracking.
 """
 import redis
 import requests
-from datetime import timedelta
+from functools import wraps
+
+r = redis.Redis()
 
 
+def url_access_count(method):
+    """Decorator for get_page function"""
+    @wraps(method)
+    def wrapper(url):
+        """Wrapper function."""
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+# Get new content and update cache
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
+        return html_content
+    return wrapper
+
+
+@url_access_count
 def get_page(url: str) -> str:
-    """
-    Returns the content of a URL after caching the requests
-    response and tracking the requests.
-    """
-    if url is None or len(url.strip()) == 0:
-        return ''
-    redis_store = redis.Redis()
-    res_key = 'result:{}'.format(url)
-    req_key = 'count:{}'.format(url)
-    result = redis_store.get(res_key)
-    if result is not None:
-        redis_store.incr(req_key)
-        return result
-    result = requests.get(url).content.decode('utf-8')
-    redis_store.setex(res_key, timedelta(seconds=10), result)
-    return result
+    """Obtains HTML content of a particular."""
+    results = requests.get(url)
+    return results.text
+
+
+if __name__ == "__main__":
+    get_page('https://slowly.robertomurray.co.uk')
